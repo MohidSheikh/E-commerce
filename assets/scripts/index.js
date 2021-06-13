948cd5622a63d88a4a26b8e51d53b6bd706ee49b
Mohid Shehzad <shehzad.mohid123@gmail.com>
2021-06-13 22:56:23 +0500

    second commit

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,10 +1,46 @@
-let menu = document.querySelector('#menu-bar');
-let navbar = document.querySelector('.navbar');
-let header = document.querySelector('.header-2');
-
-menu.addEventListener('click', () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
+window.addEventListener("scroll", function () {
+    var nav = document.querySelector("header");
+    nav.classList.toggle('sticky', window.scrollY > 0);
 });
 
 
+// var menu = document.querySelector('.menu');
+// var menubtn = document.querySelector('.menu-btn');
+// var closebtn = document.querySelector('.close-btn');
+
+// menubtn.addEventListener("click", () => {
+//     menu.classList.add('active');
+// });
+
+// closebtn.addEventListener("click", () => {
+//     menu.classList.remove("active");
+// });
+
+// $(document).ready(function(){
+//   $(".search").click(function(){
+//     $(".search-box").toggle();
+//     $("input[type=text]").focus();
+//   });
+// });
+
+$(document).ready(function() {
+     
+  $(".fa-search").click(function() {
+     $(".search-box").toggle();
+     $("input[type='text']").focus();
+   });
+
+});
+
+
+var menu = document.querySelector('.navbar');
+    var menubtn = document.querySelector('.menu-btn');
+    var closebtn = document.querySelector('.close-btn');
+
+    menubtn.addEventListener("click", () => {
+        menu.classList.add('active');
+    });
+
+    closebtn.addEventListener("click", () => {
+        menu.classList.remove("active");
+    });
